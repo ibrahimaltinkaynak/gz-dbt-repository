f8bbf5643a1d3014e8b1b27818434140b2a2350b
Ibrahim Altinkaynak <ibrahim.altinkaynak1@gmail.com>
2025-02-12 10:12:45 +0000

    column names margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,5 @@
 SELECT
-      products_id,
+      s.products_id,
       date_date,
       orders_id,
       revenue,
@@ -7,6 +7,6 @@ SELECT
       purchase_price,
       ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
       ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
-  FROM {{ref("stg_raw__sales")}} s
-  LEFT JOIN {{ref("stg_raw__product")}} p
-      USING (products_id)
+  FROM {{ref("stg_raw__sales")}} as s
+  LEFT JOIN {{ref("stg_raw__product")}} as p
+      on s.products_id=p.products_id
