fe617c5b84828600f4e64795daf897066dbcb2b5
Ibrahim Altinkaynak <ibrahim.altinkaynak1@gmail.com>
2025-02-12 14:03:26 +0000

    finance

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -5,6 +5,6 @@ sum (revenue) as revenue,
 sum (quantity) as quantity,
 sum (purchase_cost) as purchase_cost,
 sum (margin) as margin,
-from profound-coyote-448416-m4.dbt_ialtinkaynak.int_sales_margin
+from {{ref("int_sales_margin")}}
 group by date_date, orders_id
 order by date_date
--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,9 +2,16 @@ with operational_margin as (
     select
         m.orders_id,
         m.date_date,
+        m.revenue,
+        m.margin,
+        m.purchase_cost,
+        sh.shipping_fee,
+        sh.ship_cost,
+        sh.logcost,
+        m.quantity,
         m.margin + sh.shipping_fee - sh.logcost - sh.ship_cost as operational_margin
-    from profound-coyote-448416-m4.dbt_ialtinkaynak.int_sales_margin  as m
-    LEFT JOIN profound-coyote-448416-m4.dbt_ialtinkaynak.stg_raw__ship as sh on m.orders_id=sh.orders_id
+    from {{ref("int_sales_margin")}}  as m
+    LEFT JOIN {{ref("stg_raw__ship")}} as sh on m.orders_id=sh.orders_id
 ) 
 select * from operational_margin
 
