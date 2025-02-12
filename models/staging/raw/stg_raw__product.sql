9ba2159d8bde212bcda1eef929ea86c5bab3c28a
Ibrahim Altinkaynak <ibrahim.altinkaynak1@gmail.com>
2025-02-12 09:45:07 +0000

    tests unicity premary key

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -10,7 +10,7 @@ renamed as (
 
     select
         products_id,
-        CAST(purchse_price AS FLOAT64) as purchase_price
+        cast(purchse_price as FLOAT64) as purchse_price
 
     from source
 
