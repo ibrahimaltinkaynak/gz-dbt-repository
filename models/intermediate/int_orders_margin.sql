3c7ba0f92078d86ced4c001babb69c88cb41be0d
Ibrahim Altinkaynak <ibrahim.altinkaynak1@gmail.com>
2025-02-12 11:13:23 +0000

    daily

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -4,7 +4,7 @@ date_date,
 sum (revenue) as revenue,
 sum (quantity) as quantity,
 sum (purchase_cost) as purchase_cost,
-sum (margin) as margin
+sum (margin) as margin,
 from profound-coyote-448416-m4.dbt_ialtinkaynak.int_sales_margin
 group by date_date, orders_id
 order by date_date
